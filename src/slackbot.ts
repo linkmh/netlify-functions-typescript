c3472d651eda6b802965c57935111ed4a39fed60
Michael Halka <michael.halka22@gmail.com>
2023-01-06 14:48:11 -0800

    integrating Slack framework

--- src/slackbot.ts
+++ src/slackbot.ts
@@ -1,8 +1,32 @@
 import { APIGatewayEvent, Context } from 'aws-lambda'
+import { App, ExpressReceiver } from '@slack/bolt'
+import * as dotenv from 'dotenv'
+dotenv.config();
 
+const expressReceiver = new ExpressReceiver ({
+  signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
+  processBeforeResponse: true
+});
+
+const app = new App({
+  signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
+  token: `${process.env.SLACK_BOT_TOKEN}`,
+  receiver: expressReceiver
+});
+
+function parseRequestBody(stringBody: string | null) {
+  try {
+    return JSON.parse(stringBody ?? "");
+  } catch {
+    return undefined;
+  }
+}
 export async function handler (event: APIGatewayEvent, context: Context) {
-  return {
-    statusCode: 200,
-    body: "Hello, World"
-  };
+  const payload = parseRequestBody(event.body);
+  if(payload && payload.type && payload.type === 'url_verification'){
+    return {
+      statusCode: 200,
+      body: payload.challenge
+    };
+  }
 }
