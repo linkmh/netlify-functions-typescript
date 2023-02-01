bd7a30618fabf0d5b990244c8a38ade94fac7ea2
Michael Halka <michael.halka22@gmail.com>
2023-02-01 15:55:45 -0800

    Added Slack Command, Link Shared responses

--- src/slackbot.ts
+++ src/slackbot.ts
@@ -1,11 +1,12 @@
+import { APIGatewayEvent, Context } from 'aws-lambda'
+import { App, ExpressReceiver, LogLevel, ReceiverEvent, LinkUnfurls, SectionBlock } from '@slack/bolt'
+import * as HubspotClient from './hubspotClient';
+import * as Url from 'url';
+
 import * as dotenv from 'dotenv'
 dotenv.config();
 
-import { APIGatewayEvent, Context } from 'aws-lambda'
-import { App, ExpressReceiver, LogLevel, ReceiverEvent } from '@slack/bolt'
-
-
-const expressReceiver = new ExpressReceiver ({
+const expressReceiver = new ExpressReceiver({
   signingSecret: `${process.env.SLACK_SIGNING_SECRET}`,
   processBeforeResponse: true
 });
@@ -15,62 +16,174 @@ const app = new App({
   token: `${process.env.SLACK_BOT_TOKEN}`,
   receiver: expressReceiver,
   logLevel: LogLevel.DEBUG
-  
+
 });
+
+app.command('/sayhello', async ({ body, ack }) => {
+  ack();
+  await app.client.chat.postEphemeral({
+    token: process.env.SLACK_BOT_TOKEN,
+    channel: body.channel_id,
+    text: "Greetings, user!",
+    user: body.user_id
+  });
+});
+
+app.message('Hello', async ({ message }) => {
+  await app.client.chat.postMessage({
+    token: process.env.SLACK_BOT_TOKEN,
+    channel: message.channel,
+    thread_ts: message.ts,
+    text: "Hello :wave:"
+  });
+});
+
+app.event('link_shared', async ({ payload }) => {
+
+  let urls = getUrls(payload.links);
+
+  const data = parseSlackUrl(payload.links)
+
+  const unfurls: LinkUnfurls = {}
+
+  for (var key in urls) {
+    
+    const testData = await HubspotClient.getCampaignData(data[key]);
+
+    var blocks: SectionBlock[] =
+      [{
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `*<https://app.hubspot.com/contacts/4393631/record/2-7462670/${data}|${testData?.properties['campaign_name']}>*`
+        }
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: `*Status:*\n${testData?.properties['hs_pipeline_stage']}`
+          },
+          {
+            type: "mrkdwn",
+            text: `*Company:*\n${testData?.properties['company']}`
+          }
+        ]
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: `*Completed Components:*\n${testData?.properties['campaign_components_completed']}`
+          },
+          {
+            type: "mrkdwn",
+            text: `*Campaign Type:*\n${testData?.properties['campaign_type']}`
+          }
+        ]
+      },
+      {
+        type: "section",
+        fields: [
+          {
+            type: "mrkdwn",
+            text: `*Campaign Launch Date:*\n${testData?.properties['campaign_launch_date']}`
+          },
+          {
+            type: "mrkdwn",
+            text: `*Campaign End Date:*\n${testData?.properties['campaign_end_date']}`
+          }
+        ]
+      },
+      {
+        type: "section",
+        text: {
+          type: "mrkdwn",
+          text: `*Description*\n${testData?.properties['campaign_description']}`
+        }
+      }
+      ]
+
+    unfurls[`${urls[key].href}`] = {
+      color: "#fd9a32",
+      blocks
+    }
+    console.log(unfurls)
+    await app.client.chat.unfurl({
+      token: process.env.SLACK_BOT_TOKEN,
+      channel: payload.channel,
+      ts: payload.message_ts,
+      unfurls: unfurls
+    });
+  }
+
+
+})
+
+
+function parseSlackUrl(payload: any) {
+
+  var qArray = new Array<any>();
+  const links = payload
+  Object.entries(links).forEach(([key, value]) => {
+    var q = Url.parse(links[`${key}`].url)
+    qArray.push(q.pathname?.split('/').reverse()[1])
+  });
+  return qArray
+}
+
+function getUrls(payload: any) {
+  let uriArray = new Array<Url.UrlWithStringQuery>();
+  const links = payload
+  Object.entries(links).forEach(([key, value]) => {
+    var q = Url.parse(links[`${key}`].url)
+    uriArray.push(q)
+  });
+  return uriArray
+
+}
 
 function parseRequestBody(stringBody: string | null, contentType: string | undefined) {
   try {
     if (!stringBody) {
-        return "";
+      return "";
     }
 
     let result: any = {};
 
     if (contentType && contentType === "application/json") {
-        return JSON.parse(stringBody);
+      return JSON.parse(stringBody);
     }
 
     let keyValuePairs: string[] = stringBody.split("&");
     keyValuePairs.forEach(function (pair: string): void {
-        let individualKeyValuePair: string[] = pair.split("=");
-        result[individualKeyValuePair[0]] = decodeURIComponent(individualKeyValuePair[1] || "");
+      let individualKeyValuePair: string[] = pair.split("=");
+      result[individualKeyValuePair[0]] = decodeURIComponent(individualKeyValuePair[1] || "");
     });
     return JSON.parse(JSON.stringify(result));
 
-} catch {
+  }
+  catch {
     return "";
+  }
 }
-}
-export async function handler (event: APIGatewayEvent, context: Context) {
-  console.log(event.body)
-  const payload = parseRequestBody(event.body,event.headers["content-type"]);
-  console.log(payload)
-  /*if(payload && payload.type && payload.type === 'url_verification'){
+export async function handler(event: APIGatewayEvent, context: Context) {
+  const payload = parseRequestBody(event.body, event.headers["content-type"]);
+  if (payload && payload.type && payload.type === 'url_verification') {
     console.log("verified")
     return {
       statusCode: 200,
       body: payload.challenge
     };
-  }*/
+  }
 
-  app.command('/sayhello', async({body, ack}) => {
-    ack();
-    await app.client.chat.postEphemeral({
-      token: process.env.SLACK_BOT_TOKEN,
-      channel: body.channel_id,
-      text: "Greetings, user!" ,
-      user: body.user_id
-    });
-  });
-
-  app.message('Hello', async ({ message, say }) => {
-    await say("Hi :wave:");
-  });
-  
   const slackEvent: ReceiverEvent = {
     body: payload,
     ack: async (response) => {
-      return new Promise<void>((resolve,reject) => {
+      return new Promise<void>((resolve, reject) => {
+        resolve();
         return {
           statusCode: 200,
           body: response ?? ""
@@ -78,7 +191,7 @@ export async function handler (event: APIGatewayEvent, context: Context) {
       });
     },
   };
-  
+
   await app.processEvent(slackEvent);
 
   return {
